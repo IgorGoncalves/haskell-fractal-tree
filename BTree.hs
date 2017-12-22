f29a96e4733e6c0f58b32a31a1e0fc69d4ad0889
Igor Gonçalves <igor_goncalves@live.com>
2017-12-22 07:37:25 -0300

    funcional

--- BTree.hs
+++ BTree.hs
@@ -1,9 +1,4 @@
 module BTree where
-
-lerLista:: IO [Int]
-lerLista = do
-    linha <- getLine
-    return (read linha ::[Int])
     
 data BTree = Nil | Node Int BTree BTree
     deriving Show
@@ -11,13 +6,10 @@ data BTree = Nil | Node Int BTree BTree
 buildTree :: Int -> BTree -> BTree
 buildTree x Nil = Node (10-x) Nil Nil
 buildTree y (Node x esq dir) = Node (10-y) (buildTree (y-1) esq) (buildTree (y-1) dir)
-
  
 toSortedList::BTree -> [Int]
 toSortedList Nil = []
 toSortedList (Node x esq dir) = toSortedList esq ++ [x] ++ toSortedList dir
-
-
 
 nodeCount::BTree -> Int
 nodeCount Nil = 0
--- Main.hs
+++ Main.hs
@@ -1,32 +1,54 @@
 module Main where
 
 import Graphics.Gloss
-import BTree
 import Data.List
 
-window :: Display
-window = InWindow "Nice Window" (800, 800) (10, 10)
+data BTree = Nil | Node Int BTree BTree
+    deriving Show
 
-edge = 360 :: Float
+-- definição da escala 
+treeScale:: Float 
+treeScale = 0.7
 
-background :: Color
-background = white
+-- metodo para preenchimento de arvore binaria sem risco desbalanceamento
+buildTree :: Int -> BTree -> BTree
+buildTree x Nil = Node (10-x) Nil Nil
+buildTree y (Node x esq dir) = Node (10-y) (buildTree (y-1) esq) (buildTree (y-1) dir)
 
+-- união dos pictures e formação de um contexto de pintura
 drawing :: Picture
-drawing = Line [(0, 0), (0, 200)]
+drawing = Pictures lines
+        where            
+            arvore = foldr buildTree Nil (reverse [1..10]) -- invocação e preenchimento da arvore modelo
+            lines  = buildLinesTree arvore [(0,0),(0, 50)] (-30) -- cria as linhas com base na arvore modelo
 
--- drawTree:: BTree -> Picture
--- drawTree bt = Pictures 
 
-toPicturesList::BTree -> [Picture]
-toPicturesList Nil = [Blank]
-toPicturesList (Node x esq dir) = toPicturesList esq ++ [Rotate (fromIntegral(x)*30) (Line [(x*50, x*50), (0, 200)])] ++ toPicturesList dir
+-- definição de lista contendo o desenho de cada linha da arvore
+buildLinesTree:: BTree -> Path -> Float -> [Picture]
+buildLinesTree Nil _ _ = [Blank]
+buildLinesTree (Node x esq dir) [pInit, pFin] angPrincipal = buildLinesTree esq atual (angPrincipal+30) ++ [Line atual] ++ buildLinesTree dir atual (angPrincipal-30)
+    where        
+        -- a linha atual é definida com base na posição a linha anterior e o angulo acrescido de 30 graus
+        atual =  rotateLine (angPrincipal+30) (nextLine [pInit, pFin])
+
+-- baseia a posição da nova linha usando com base o ramo anterior
+nextLine:: Path -> Path
+nextLine [(x1, y1),(x2, y2)] = [(x2, y2), (x2, y2 + lsize)]
+            where 
+                -- mede o tamanho do ramo anterior e multiplica pela escala
+                lsize = sqrt(((x2-x1)^2)+((y2-y1)^2)) * treeScale
+
+-- gira a reta 
+rotateLine::Float -> Path -> Path
+rotateLine ang [(x1,y1), (x2,y2)]
+    = [(x1, y1), (x' + x1, y' + y1)]
+    where
+        alpha = ang * pi / 180;
+        x0 = x2 - x1
+        y0 = y2 - y1
+        x' = x0 * cos alpha - y0 * sin alpha
+        y' = x0 * sin alpha + y0 * cos alpha
+
 
 main :: IO ()
-main =  do
-    let lista = [1..9]
-    let arvore = foldr buildTree Nil (reverse lista)         
-    print ( sort (toSortedList arvore))
-    print (treeLevel arvore)
-    print (nodeCount arvore)
-    display window background (Pictures (toPicturesList arvore))
+main = display (InWindow "Nice Window" (800, 800) (10, 10)) white drawing
